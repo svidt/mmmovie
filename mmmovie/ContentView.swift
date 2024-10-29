7da0f021253e9a1cbf60f469d415888ea9f90aee
Kristian Emil <79602784+svidt@users.noreply.github.com>
2024-10-29 07:58:18 +0100

    initial files

---
+++ mmmovie/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  mmmovie
+//
+//  Created by Kristian Emil on 29/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, CS50!")
+            Text("Hello, CS50!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mmmovie/mmmovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  mmmovieApp.swift
+//  mmmovie
+//
+//  Created by Kristian Emil on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct mmmovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
